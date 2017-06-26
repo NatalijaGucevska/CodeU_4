75edf9deb4aeae3d19bd38192cfb1aea48c30978
NatalijaGucevska <natalija.gucevska@epfl.ch>
2017-06-26 15:36:29 +0200

    Count islands using disjoint set

--- Map.java
+++ Map.java
@@ -1,60 +1,60 @@
-import java.util.ArrayList;
-import java.util.Collections;
-
 public class Map {
-	boolean map[][]; 
-	
+	boolean map[][];
+
 	public Map(boolean[][] map) {
-		if(map == null) {
-			throw new IllegalArgumentException(); 
+		if (map == null) {
+			throw new IllegalArgumentException();
 		}
-		this.map = new boolean[map.length][map[0].length]; 
-		for(int i=0; i<map.length; i++) {
-			for(int j=0; j<map[0].length; j++){
-				this.map[i][j] = map[i][j]; 
+		this.map = new boolean[map.length][map[0].length];
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[0].length; j++) {
+				this.map[i][j] = map[i][j];
 			}
 		}
 	}
-	
+
 	public int countIslands() {
-		int helperMap[][] = new int[map.length][map[0].length]; 
-		int count = 0; 
-		boolean stable = false; 
-		
-		while(!stable) {
-			stable = true; 
-			for(int i=0; i<map.length; i++) {
-				for(int j=0; j<map[0].length; j++){
-					ArrayList<Integer> neighbors = getNeighbors(helperMap, i, j); 
-					if(map[i][j]) {
-						if(!neighbors.isEmpty()) {
-							int min = Collections.min(neighbors);
-							if(helperMap[i][j] != 0 && min!=helperMap[i][j]) {
-								stable = false;
-							}
-							helperMap[i][j] =  min;
-						} else if(helperMap[i][j] == 0) {
-							count++;
-							helperMap[i][j] = count; 
-							stable=false; 
-						}
+		int helperMap[][] = new int[map.length][map[0].length];
+		int count = 0;
+
+		// Rough estimation of connected components
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[0].length; j++) {
+				if (map[i][j]) {
+					if (upperTile(helperMap, i, j) > 0) {
+						helperMap[i][j] = upperTile(helperMap, i, j);
+					} else if (leftTile(helperMap, i, j) > 0) {
+						helperMap[i][j] = leftTile(helperMap, i, j);
+					} else {
+						count++;
+						helperMap[i][j] = count;
 					}
 				}
-			}	
+			}
 		}
-		
-		//TODO: change
-		return count;  
+
+		// Merge all connected components
+		DisjointSet set = new DisjointSet(count);
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[0].length; j++) {
+				if (map[i][j]) {
+					int up = upperTile(helperMap, i, j);
+					int left = leftTile(helperMap, i, j);
+					if (up > 0 && left > 0 && up != left) {
+						set.union(up, left);
+					}
+				}
+			}
+		}
+
+		return set.countDisjointSets();
 	}
-	
-	private ArrayList<Integer> getNeighbors(int[][] map, int i, int j) {
-		ArrayList<Integer> result = new ArrayList<>(); 
-		int width = map[0].length; 
-		
-	    if (i > 0 && map[i-1][j]!=0) result.add(map[i-1][j]);
-	    if (j > 0 && map[i][j-1]!=0) result.add(map[i][j-1]);
-	    if (j < width - 1 && map[i][j+1]!=0) result.add(map[i][j + 1]);
-	    
-		return result; 
+
+	private int leftTile(int[][] map, int i, int j) {
+		return i > 0 ? map[i - 1][j] : 0;
+	}
+
+	private int upperTile(int[][] map, int i, int j) {
+		return j > 0 ? map[i][j - 1] : 0;
 	}
 }
